aaa53f84dafa7f33854e20b248579756ba606df3
yongjoo <yongjool@missionreadyhq.com>
2025-01-06 20:01:51 +1300

    added footer mobile

--- frontend/trademe-app/src/components/Footer/Footer.tsx
+++ frontend/trademe-app/src/components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Footer.module.css'; // Import styles as an object
-import footer from '../../assets/Footer nav.png'; // Import the logo image
+import footer from '../../assets/Footer nav.png';
+import footerMobile from '../../assets/Mobile footer nav.png';
 
 const Footer: React.FC = () => {
     return (
-        <div className={styles.footer}>
-            {footer && <img src={footer} alt="footer" />}
+        <div>
+            <div className={`${styles.footer} ${styles.desktop}`}>
+                {footer && <img src={footer} alt="footer" />}
+            </div>
+            <div className={`${styles.footer} ${styles.mobile}`}>
+                {footerMobile && <img src={footerMobile} alt="footerMobile" />}
+            </div>
         </div>
     );
 };
