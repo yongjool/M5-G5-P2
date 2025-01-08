c2d9c9743be7dca07611dea033d0310c17fe523b
Sam H <samuelh@missionreadyhq.com>
2025-01-09 12:53:15 +1300

    CSS reset

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListingPage from "./pages/ListingPage";
-// ...existing code...
+import "./App.css";
 
 const App: React.FC = () => {
   return (
