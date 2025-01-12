890fa70849b0dd9081b93ac0bb42197c080d8eac
Sam H <samuelh@missionreadyhq.com>
2025-01-12 15:42:35 +1300

    fix db seed

--- backend/src/scripts/seedScript.js
+++ backend/src/scripts/seedScript.js
@@ -1,23 +1,21 @@
-/* mySeedScript.js */
+require('dotenv').config();
 
 // require the necessary libraries
-const faker = require("faker");
+const { faker } = require('@faker-js/faker');
 const MongoClient = require("mongodb").MongoClient;
 
 async function seedDB() {
+    require('dotenv').config();
     // Connection URL
-    const uri = process.env("MONGO_URI")
+    const uri = process.env.MONGO_URI
 
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        // useUnifiedTopology: true,
-    });
+    const client = new MongoClient(uri);
 
     try {
         await client.connect();
         console.log("Connected correctly to server");
 
-        const collection = client.db("iot").collection("Listing");
+        const collection = client.db("app").collection("Listing");
 
         // The drop() command destroys all data from a collection.
         // Make sure you run it against proper database and collection.
@@ -26,23 +24,18 @@ async function seedDB() {
         // make a bunch of time series data
         let listingData = [];
 
-        title: string;
-        seller: user;
-        pageViews: number;
-        id: string;
-
 
         for (let i = 0; i < 5000; i++) {
             const title =  faker.commerce.productName();
-            const seller =faker.name.firstName() + faker.name.lastName();
+            const seller =faker.person.firstName() + " " + faker.person.lastName();
             const pageviews = faker.number.int();
             const  watchlistCount= faker.number.int();
             const viewCount = faker.number.int();
             const images = [faker.image.url(), faker.image.url(), faker.image.url()]
             const createdAt = faker.date.recent();
-            listingData.push({title, seller, pageviews, watchlistCount, viewCount, images})
+            listingData.push({title, seller, pageviews, watchlistCount, viewCount, images, createdAt})
         }
-        collection.insertMany(listingData);
+        await collection.insertMany(listingData);
 
         console.log("Database seeded! :)");
         client.close();
