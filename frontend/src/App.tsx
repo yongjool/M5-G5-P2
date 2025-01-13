e9ab6392a756c5f7bfd97498582fb006eba6763f
Sam H <samuelh@missionreadyhq.com>
2025-01-14 12:38:43 +1300

    Fix import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage'; // TypeScript
 import ListingPage from './pages/ListingPage';
 import SearchResultPage from './pages/SearchResultPage'; // JavaScript
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
 
 const queryClient = new QueryClient();
 
